bb9286ab5de9333efcfdba55727f4871b2cb4e39
matheussASR <rm551466@fiap.com>
2023-11-17 11:32:47 -0300

    fix: corrigindo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,38 +3,48 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export const Header = () => {
-    const [userInfo, setUserInfo] = useState({})
-    const navigate = useNavigate()
-    const userInfos = sessionStorage.getItem("@USERINFO:MindWell")
+  const [userInfo, setUserInfo] = useState({});
+  const navigate = useNavigate();
+  const userInfos = sessionStorage.getItem("@USERINFO:MindWell");
 
-    const logout = () => {
-        sessionStorage.removeItem("@USERINFO:MindWell")
-        toast.success("Usuário deslogado com sucesso!");
-        setTimeout(() => {
-            navigate("/");
-        }, 2000);
+  const logout = () => {
+    sessionStorage.removeItem("@USERINFO:MindWell");
+    toast.success("Usuário deslogado com sucesso!");
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (userInfos) {
+      const parsedUserInfos = JSON.parse(userInfos);
+      setUserInfo(parsedUserInfos);
     }
+  }, [userInfos]);
 
-    useEffect(() => {
-        if (userInfos) {
-            const parsedUserInfos = JSON.parse(userInfos)
-            setUserInfo(parsedUserInfos)
-        }
-      }, [userInfos]);
-
-    return (
-        <>
-            <header className="header__container">
-                {userInfos ? (
-                    <>
-                        <p className="header__username">OLÁ {userInfo.username ? userInfo.username.toUpperCase() : null}</p>
-                        <p className="header__email">{userInfo.email}</p>
-                        <button className="header__logout" onClick={() => logout()}>Logout</button>
-                    </>
-                ) : (
-                    <p className="header__title">MindWell - Acompanhamento e Cuidado Pessoal</p>
-                )}
-            </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="header__container">
+        {userInfos ? (
+          <>
+            <p className="header__username">
+              OLÁ {userInfo.username ? userInfo.username.toUpperCase() : null}
+            </p>
+            <p className="header__email">{userInfo.email}</p>
+            <p className="header__email">Global Solution 2023 - Engenharia de Software</p>
+            <button className="header__logout" onClick={() => logout()}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="header__title">
+              MindWell - Acompanhamento e Cuidado Pessoal
+            </p>
+            <p className="header__email">Global Solution 2023 - Engenharia de Software</p>
+          </>
+        )}
+      </header>
+    </>
+  );
+};
